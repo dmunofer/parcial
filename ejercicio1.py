f9497761bfda51901aaed19b24209f2840f8c50e
dmunofer <diego.fmz@hotmail.com>
2021-12-14 11:35:28 +0100

    commit final ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+def minion_game():
+    str= input('Introduce la palabra para jugar: ')
+    kevin = 0
+    stuart = 0
+    vocales = 'AEIOU'
+
+    for e in range((len(str))):
+        if str[e] in vocales:
+            kevin+= (len(str)-e)
+        else:
+            stuart+=(len(str)-e)
+    if stuart > kevin :
+        print('Stuart wins with', stuart, 'points')
+    elif kevin > stuart:
+        print('Kevin wins with', kevin, 'points')
+    else:
+        print('Draw')
--- ejercicio2.py
+++ ejercicio2.py
@@ -6,10 +6,26 @@ import sys
 
 def verticalRooks(r1, r2):
     or_exclusivo = 0
-    for x, y in zip(r1,r2):
-        or_exclusivo^= (abs(x-y)-1)
+    for x, y in zip(r1, r2):
+        or_exclusivo ^= (abs(x-y) - 1)
     if or_exclusivo!=0:
-        return 'player 2'
+        return 'Player 2'
     else:
-        return 'player 1'
+        return 'Player 1'
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    t = int(input().strip())
+    for t_itr in range(t):
+        n = int(input().strip())
+        r1 = []
+        for _ in range(n):
+            r1_item = int(input().strip())
+            r1.append(r1_item)
+        r2 = []
+        for _ in range(n):
+            r2_item = int(input().strip())
+            r2.append(r2_item)
+        result = verticalRooks(r1, r2)
+        fptr.write(result + '\n')
+    fptr.close()
